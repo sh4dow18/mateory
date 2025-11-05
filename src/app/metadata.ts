f1ee391656e71f60c57f016c30b0291f0bfcd466
sh4dow18 <ramses.solano.arias@gmail.com>
2025-11-05 10:58:41 -0600

    chore: add routes map to have only one source of truth

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -1,6 +1,7 @@
 // Home Metadata Requirements
+import { ROUTES_MAP } from "@/shared/config/routes";
 import type { Metadata } from "next";
 // Home Metadata
 export const HOME_METADATA: Metadata = {
-  title: "Inicio",
+  title: ROUTES_MAP[""].title,
 };
--- src/shared/config/routes.ts
+++ src/shared/config/routes.ts
@@ -16,13 +16,14 @@ export const ROUTES_LIST: Route[] = [
   {
     path: "",
     title: "Inicio",
-    summary: "Página Principal de Mateory",
+    summary:
+      "Mateory es una herramienta web que simplifica la resolución de problemas de Teorías de Inventarios y Teorías de Colas. Optimice sus cálculos y visualice las fórmulas detrás de cada modelo.",
     Icon: FaHome,
     inSitemap: true,
     inHome: false,
   },
   {
-    path: "inventary",
+    path: "inventories",
     title: "Inventarios",
     summary:
       "Calcula modelos de inventarios como EOQ con o sin déficit, optimizando sus decisiones.",
@@ -64,3 +65,5 @@ export const ROUTES_LIST: Route[] = [
     inHome: false,
   },
 ];
+// Map that allows to get page data easier
+export const ROUTES_MAP = Object.fromEntries(ROUTES_LIST.map((route) => [route.path, route]));
