53ccb237eb7e6ed92820c8755e1be87953ef282f
sh4dow18 <ramses.solano.arias@gmail.com>
2025-11-10 08:53:29 -0600

    chore(inventories): change currency symbol reference to barrel

--- src/features/inventories/lib/shared/get-account-number.ts
+++ src/features/inventories/lib/shared/get-account-number.ts
@@ -1,5 +1,5 @@
 //  Get Account Number Requirements
-import { CURRENCY_SYMBOL } from "@/shared/config/currencies";
+import { CURRENCY_SYMBOL } from "../../config";
 // Returns a string number in accounting format
 // Example: "12000" to "$12,000.00"
 export function GetAccountNumber(currency: number, number: number): string {
