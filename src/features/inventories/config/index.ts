ee38dc8f9f2c7e1bc41fae107e029ffa4c559758
sh4dow18 <ramses.solano.arias@gmail.com>
2025-11-10 08:47:31 -0600

    chore: add results config list and currencies in barrel

--- src/features/inventories/config/index.ts
+++ src/features/inventories/config/index.ts
@@ -1,3 +1,5 @@
 export { GetDisabledInventoryModels, MODELS_IDS, MODELS_LIST } from "./models";
 export { GetEnabledInventoryResults, RESULTS_LIST } from "./results";
 export { VARIABLES_LIST } from "./variables";
+export { RESULTS_CONFIG_LIST } from "./results-settings";
+export { CURRENCIES_IDS, CURRENCY_SYMBOL, CURRENCIES_LIST } from "./currencies";
