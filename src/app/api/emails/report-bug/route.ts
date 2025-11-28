1a6ed995a491f098124796325edbbc70eec7a23c
sh4dow18 <ramses.solano.arias@gmail.com>
2025-11-28 13:41:42 -0600

    chore: sharp hotfix in vercel

--- src/app/api/emails/report-bug/route.ts
+++ src/app/api/emails/report-bug/route.ts
@@ -1,5 +1,7 @@
 // Report Bug Route Requirements
 import { ReportBug } from "@/modules/emails/services";
+// Force Node JS Runtime
+export const runtime = "nodejs";
 // Report Bug Post Function
 export async function POST(request: Request) {
   return ReportBug(request);
