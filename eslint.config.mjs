27accf3f5b4116a10cdb36377c81b70d841f239e
sh4dow18 <ramses.solano.arias@gmail.com>
2025-12-10 14:05:31 -0600

    docs: add storybook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import storybook from "eslint-plugin-storybook";
 // Get the current file URL as a normal system path
 const FILEPATH = fileURLToPath(import.meta.url);
 // Get the Directory's name from the File Path
@@ -11,19 +12,16 @@ const COMPAT = new FlatCompat({
   baseDirectory: DIRECTORY_NAME,
 });
 // ESLint Main Config
-const ESLINT_CONFIG = [
-  // Import NextJS and Typescript default rules
-  ...COMPAT.extends("next/core-web-vitals", "next/typescript"),
-  // Ignore this file in check
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-  },
-];
+const ESLINT_CONFIG = [// Import NextJS and Typescript default rules
+...COMPAT.extends("next/core-web-vitals", "next/typescript"), // Ignore this file in check
+{
+  ignores: [
+    "node_modules/**",
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ],
+}, ...storybook.configs["flat/recommended"]];
 
 export default ESLINT_CONFIG;
